3c45adc26e1890ef50273d06f147359653e6255c
Marcin Fura <m.fura@oberthur.com>
2017-07-17 10:16:12 +0200

    +

---
+++ LDAP/add-user-to-group-module.sh
@@ -0,0 +1,32 @@
+echo -e "\\033[32m\n###########################\n###	add user to group module	###\n###########################\n\\033[0m"
+TMP_FILE=add_user_to_group.ldif
+echo -e "*** Czyszczenie plikow podrecznych - $TMP_FILE ***"
+echo "" > $TMP_FILE
+echo $TMP_FILE
+
+echo -e "\n\n\t########################################"
+
+for (( i=2; i <= $L_WIERSZY+1; i++ )) ; do
+	CN=`awk -v avar="$i" 'NR==avar{print $LICZBA1}' USERS.txt`;
+
+	TAB=( $CN )
+	IMIE=`echo ${TAB[0]}`
+	NAZWISKO=`echo ${TAB[1]}`
+	#Wszystko malymi literami
+	IMIE=`echo "$IMIE" | tr '[:upper:]' '[:lower:]'`
+	NAZWISKO=`echo "$NAZWISKO" | tr '[:upper:]' '[:lower:]'`
+	#Imie i nazwisko rozpoczynajac z duzych liter
+	IMIE="${IMIE[@]^}"
+	NAZWISKO="${NAZWISKO[@]^}"
+
+	cat >> add_user_to_group.ldif << EOF
+dn: cn=jira-users,ou=services,ou=groups,dc=otlabs
+changetype: modify
+add: uniqueMember
+uniqueMember: cn=$IMIE $NAZWISKO,ou=SBU,ou=Users,dc=otlabs
+EOF
+
+done
+
+#ldapmodify -h 10.122.202.15 -p 389 -D cn=admin,dc=otlabs -w dupa.123 -f group.ldif
+ldapmodify -h $HOST -p $PORT -D cn=admin,dc=otlabs -w $PASS -f $TMP_FILE
